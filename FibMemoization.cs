5fda9a705642e15ef89d38fb867f07b9317960f6
ahmed ayari <ayari17@hotmail.fr>
2021-05-02 16:52:00 +0100

    Add project files.

---
+++ FibMemoization.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace DynamicProgramming
+{
+    class FibMemoization
+    {
+        //Time : 2^n
+        //Space : n
+        private static readonly Dictionary<int, long> dict = new Dictionary<int, long>()
+        {
+            {0,0},{1,1}
+        };
+        public static long Fib(int n)
+        {
+            if (dict.ContainsKey(n)) return dict[n];
+            var value = Fib(n - 1) + Fib(n - 2);
+            dict.Add(n, value);
+            return value;
+        }
+
+        public static void Run()
+        {
+            Console.Write("n = ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Fib({n}) = {Fib(n)}");
+
+        }
+    }
+}
